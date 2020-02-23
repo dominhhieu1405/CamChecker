cf33d238da3c13a539955fda59165ebcb547cfc7
dominhhieu1405 <51281387+dominhhieu1405@users.noreply.github.com>
2020-02-23 07:12:38 +0700

    Add files via upload

---
+++ DS/hk.py
@@ -0,0 +1,54 @@
+
+import requests,os,re
+
+b="\033[0;34m"
+g="\033[1;32m"
+w="\033[1;37m"
+r="\033[1;31m"
+y="\033[1;33m"
+cyan = "\033[0;36m"
+lgray = "\033[0;37m"
+dgray = "\033[1;30m"
+ir = "\033[0;101m"
+reset = "\033[0m"
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+
+def unitedstates():
+    global page
+    res = requests.get('https://www.insecam.org/en/bycountry/HK/', headers=headers)
+    findpage = re.findall('"?page=",\s\d+', res.text)[1]
+    rfindpage = findpage.replace('page=", ', '')
+    os.system('clear')
+    print("{}        ____ ").format(r)
+    print("   _[]_/____\__n_ ")
+    print("  |_____.--.__()_|")
+    print("  |I   //# \\\    |")
+    print("{}  |P   \\\__//    | ").format(w)
+    print("  |CS   '--'     | ")
+    print("{}  '--------------'----------{}------------------.  ").format(r,w)
+    print("{}  | {}Author  : {}MHit {}     | {}VIE{}T{}{}NAM         | ").format(r,w,r,w,r,ir,reset,w)
+    print("{}  | {}Youtube : {}???? {}| {}+84-xxx-xxx-xxx {}|").format(r,w,w,w,lgray,w)
+    print("{}  '------------------------------------{}-------'  ").format(r,w)
+    print("{}       [ {}Tong So Trang : {} {}]").format(r,w,rfindpage,r)
+    run()
+    
+def run():
+    try:
+        page = input("\033[1;31m       [ \033[1;37mChon trang \033[1;31m]\033[1;37m> ")
+        url = ("https://www.insecam.org/en/bycountry/HK/?page="+str(page))
+        print ""
+        res = requests.get(url, headers=headers)
+        findip = re.findall('http://\d+.\d+.\d+.\d+:\d+', res.text)
+        count = 1
+        for _ in findip:
+             hasil = findip[count]
+             print ("{}[ {}{} {}]").format(r,w,hasil,r)
+             count += 1
+    except:
+        print ""
+        print r+"Hãy sao chép một trong các địa chỉ trên và dán vào trình duyệt"+w
+        print r+"Please copy a URL on it and paste in your browse"+w 
